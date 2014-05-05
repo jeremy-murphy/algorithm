d71c9b592a2cc2be2b4edc9f0d83a05467c5d408
Jeremy W. Murphy <jeremy.william.murphy@gmail.com>
2014-05-06 00:33:36 +1000

    Switched to C++11 because it's just silly not using it.

--- speed_test.cpp
+++ speed_test.cpp
@@ -6,10 +6,8 @@
 #include <boost/algorithm/integer_sort.hpp>
 
 #include <boost/lexical_cast.hpp>
-#include <boost/random.hpp>
-#include <boost/timer/timer.hpp>
-#include <boost/bind.hpp>
 
+#include <chrono>
 #include <iostream>
 #include <vector>
 #include <functional>
@@ -19,28 +17,26 @@
 #include <iomanip>
 #include <typeinfo>
 #include <cmath>
+#include <random>
+
+using namespace std;
 
 using boost::lexical_cast;
-using namespace boost::random;
 using namespace boost::algorithm;
 
 
 template <typename T>
 struct foo
 {
-    static mt19937 rng;
     T key;
-    std::string s;
-    foo() : key(T()), s(std::string()) {}
-    foo(T const &key, std::string const &s) : key(key), s(s) {}
-    foo(T const &key, char const *s) : key(key), s(std::string(s)) {}
-    foo(T const &key) : key(key), s("satellite") {}
-    /*
-    // Although this particular foo is slower, it is better for testing.
-    foo() : key(T()), d(rng()) {}
-    foo(T const &key) : key(key), d(rng()) {}
-    U d;
-    */
+    double d;
+    foo() : key(T()), d(double()) {}
+    foo(T x) : key(x), d(double()) {}
+
+    operator T() const
+    {
+        return key;
+    }
     
     
     friend
@@ -53,7 +49,7 @@ struct foo
     friend
     bool operator==(foo const &a, foo const &b)
     {
-        return a.key == b.key && a.s == b.s;
+        return a.key == b.key;
     }
     
     
@@ -68,57 +64,64 @@ struct foo
     };
 };
 
-template <typename T>
-mt19937 foo<T>::rng = mt19937();
+
+template <typename F, typename I>
+chrono::duration<double, nano> time_sort(F sort, I first, I last, char unsigned logn, unsigned max_logn)
+{
+    auto const reps = 1ul << (max_logn - logn);
+    vector<typename iterator_traits<I>::value_type> buffer(first, last);
+    chrono::duration<double, nano> elapsed;
+    for(long unsigned i = 0; i < reps; i++)
+    {
+        auto const start = chrono::high_resolution_clock::now();
+        sort(begin(buffer), end(buffer));
+        elapsed += chrono::high_resolution_clock::now() - start;
+        copy(first, last, begin(buffer));
+    }
+    return elapsed / reps;
+}
+
 
 template <typename T, class Distribution>
 void test(Distribution dist, unsigned const seed = 0, unsigned const max2 = 26)
 {
-    // typedef typename std::vector<T>::iterator iterator;
-    typedef T value_type;
-    typedef transformation::identity<T> converter;
-    typedef typename std::vector<value_type>::const_iterator const_iterator;
+    typedef foo<T> value_type;
+    typedef transformation::implicit<T> converter;
+    typedef typename vector<value_type>::iterator v_iterator;
+    typedef typename vector<value_type>::const_iterator v_const_iterator;
 
-    std::cout << "=== Test (seed = " << seed << ", T = " << typeid(T).name() << "). ===" << std::endl;
+    cout << "=== Test (seed = " << seed << ", T = " << typeid(T).name() << "). ===" << endl;
     mt19937 rng(seed);
     converter const conv;
 
-    std::cout << "log2(n)\tt/n (ns)\tc\tx sort\tx stable_sort\n";
-    std::cout << "-------------------------------------------------------------\n";
-    for(unsigned p = 16; p <= max2; p++)
+    cout << "log2(n)\tt/n (ns)\tc\tx sort\tx stable_sort\n";
+    cout << "-------------------------------------------------------------\n";
+    for(unsigned p = 1; p <= max2; p++)
     {
-        std::size_t const n(1ul << p);
-        std::vector<value_type> A;
+        size_t const n(1ul << p);
+        vector<value_type> A;
         A.reserve(n);
-        std::generate_n(std::back_inserter(A), n, boost::bind(dist, rng));
-        std::vector<value_type> B(A);
-        const_iterator const    _min = std::min_element(A.begin(), A.end()), 
-                                _max = std::max_element(A.begin(), A.end());
-        std::cout << std::setw(7) << p << "\t";
-        std::cout.flush();
-        std::vector<value_type> X(A);
-        boost::timer::cpu_timer timer;
-        stable_radix_sort(A.begin(), A.end(), B.begin(), conv, conv(*_min), conv(*_max));
-        boost::timer::cpu_times const t1 = timer.elapsed();
-        std::sort(X.begin(), X.end());
-        boost::timer::cpu_times const t2 = timer.elapsed();
-        std::stable_sort(A.begin(), A.end());
-        boost::timer::cpu_times const t3 = timer.elapsed();
-        timer.stop();
-        boost::timer::nanosecond_type const     t_radix((t1.system + t1.user));
-        if(t_radix != 0)
-        {
-            boost::timer::nanosecond_type const t_sort(t2.system + t2.user - t_radix),
-                                                t_stable_sort(t3.system + t3.user - t2.system - t2.user);
-            std::cout.precision(2);
-            std::cout.setf(std::ios::fixed);
-            std::cout << std::setw(8) << t_radix / static_cast<double>(n) << "\t";
-            std::cout << t_radix / n / log2(n) << "\t";
-            std::cout.precision(1);
-            std::cout << static_cast<double>(t_sort) / t_radix << "\t";
-            std::cout << static_cast<double>(t_stable_sort) / t_radix;
-        }
-        std::cout << std::endl;
+        generate_n(back_inserter(A), n, bind(dist, rng));
+        vector<value_type> B(A);
+        auto const  _min = min_element(begin(A), end(A)), 
+                    _max = max_element(begin(A), end(A));
+        cout << setw(7) << p << "\t" << flush;
+        vector<value_type> X(A);
+        auto const f1 = bind(static_cast<v_iterator(*)(v_const_iterator, v_const_iterator, v_iterator, converter, T, T, unsigned)>(stable_radix_sort<v_const_iterator, v_iterator, converter>), placeholders::_1, placeholders::_2, begin(B), conv, conv(*_min), conv(*_max), 8u);
+        auto const f2 = bind(sort<v_iterator>, placeholders::_1, placeholders::_2);
+        auto const f3 = bind(stable_sort<v_iterator>, placeholders::_1, placeholders::_2);
+        auto const t1 = time_sort(f1, begin(A), end(A), p, max2);
+        auto const t2 = time_sort(f2, begin(A), end(A), p, max2);
+        auto const t3 = time_sort(f3, begin(A), end(A), p, max2);
+
+        cout.precision(2);
+        cout.setf(ios::fixed);
+        cout << setw(8) << t1.count() / n << "\t";
+        cout << t1.count() / n / log2(n) << "\t";
+        cout.precision(1);
+        cout << t2.count() / t1.count() << "\t";
+        cout << t3.count() / t1.count();
+        cout << endl;
     }
 }
 
@@ -127,22 +130,23 @@ void test(Distribution dist, unsigned const seed = 0, unsigned const max2 = 26)
 int main(int argc, char **argv)
 {
     // Configure cout to my liking.
-    std::ios_base::sync_with_stdio(false);
-    std::locale::global(std::locale(""));
-    std::cout.imbue(std::locale());
+    ios_base::sync_with_stdio(false);
+    locale::global(locale(""));
+    cout.imbue(locale());
     
-    unsigned const seed(argc < 2 ? std::time(NULL) : lexical_cast<unsigned>(argv[1]));
-    
-    std::cout << "\n*** poisson_distribution, mean = std::numeric_limits<T>::max() / 2" << std::endl;
-    test<unsigned char>(poisson_distribution<unsigned char>(std::numeric_limits<unsigned char>::max() / 2), seed, 28);
-    test<unsigned short>(poisson_distribution<unsigned short>(std::numeric_limits<unsigned short>::max() / 2), seed, 28);
-    test<unsigned int>(poisson_distribution<unsigned int>(std::numeric_limits<unsigned int>::max() / 2), seed);
-    test<unsigned long>(poisson_distribution<unsigned long>(std::numeric_limits<unsigned long>::max() / 2), seed);
-    
-    std::cout << "\n*** uniform_int_distribution, min = 0, k = std::numeric_limits<T>::max()" << std::endl;
-    test<unsigned char>(uniform_int_distribution<unsigned char>(0, std::numeric_limits<unsigned char>::max()), seed, 28);
-    test<unsigned short>(uniform_int_distribution<unsigned short>(0, std::numeric_limits<unsigned short>::max()), seed, 28);
-    test<unsigned int>(uniform_int_distribution<unsigned int>(0, std::numeric_limits<unsigned int>::max()), seed);
-    test<unsigned long>(uniform_int_distribution<unsigned long>(0, std::numeric_limits<unsigned long>::max()), seed);
+    unsigned const  seed(argc < 2 ? time(NULL) : lexical_cast<unsigned>(argv[1])),
+                    max_logn(argc < 3 ? 24 : stoul(argv[2]));
+
+    cout << "\n*** poisson_distribution, mean = numeric_limits<T>::max() / 2" << endl;
+    test<unsigned char>(poisson_distribution<unsigned char>(numeric_limits<unsigned char>::max() / 2), seed, max_logn);
+    test<unsigned short>(poisson_distribution<unsigned short>(numeric_limits<unsigned short>::max() / 2), seed, max_logn);
+    test<unsigned int>(poisson_distribution<unsigned int>(numeric_limits<unsigned int>::max() / 2), seed, max_logn);
+    test<unsigned long>(poisson_distribution<unsigned long>(numeric_limits<unsigned long>::max() / 2), seed, max_logn);
+
+    cout << "\n*** uniform_int_distribution, min = 0, k = numeric_limits<T>::max()" << endl;
+    test<unsigned char>(uniform_int_distribution<unsigned char>(0, numeric_limits<unsigned char>::max()), seed, max_logn);
+    test<unsigned short>(uniform_int_distribution<unsigned short>(0, numeric_limits<unsigned short>::max()), seed, max_logn);
+    test<unsigned int>(uniform_int_distribution<unsigned int>(0, numeric_limits<unsigned int>::max()), seed, max_logn);
+    test<unsigned long>(uniform_int_distribution<unsigned long>(0, numeric_limits<unsigned long>::max()), seed, max_logn);
     return 0;
 }
